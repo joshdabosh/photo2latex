84e3fecf73b7b387c2f4c417bb2b9c7b12238705
Qwerty71 <ganiareeb@gmail.com>
2020-10-30 13:23:26 -0400

    Installed Bootstrap, testing button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button> Testing </Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
