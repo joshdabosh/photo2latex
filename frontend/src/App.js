b715d3781efea5a9c3cf7fd5f837a948c2bf3426
Qwerty71 <ganiareeb@gmail.com>
2020-10-30 18:08:13 -0400

    Added Navbar, started front page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation></Navigation>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Photo2LaTex</h1>
+            <p>A free, online, convenient tool to to convert your handwritten papers to LaTex 
+            code, with the click of a button.</p>
+          </div>
+
+          <Button>Learn More</Button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <Button> Testing </Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+
+function Navigation(){
+    return (
+        <Container>
+        <div class="bs-example">
+    <ul class="nav nav-pills nav-fill w-100">
+        <li class="nav-item">
+            <a class="nav-link active">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link">Convert</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link">Signup</a>
+        </li>
+    </ul>
+</div>
+</Container>
+    );
+}
+
+export default Navigation;
